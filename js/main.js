53e37bad7239e1538da4981e56e033d450ed9315
Gisel Paola Olivares <giselpolivares@Gisels-MacBook-Pro.local>
2017-02-01 07:43:26 +0100

    added a new branch called presidents

--- js/main.js
+++ js/main.js
@@ -15,3 +15,50 @@ var styles = [
 
 console.log("Git's %cpull%c is awesome", styles, '');
 console.log('Check out the docs here https://git-scm.com/docs/git-pull');
+
+var iceCream = [
+    ['cookie dough', 'strawberry', 'vanilla'],
+    'mango',
+];
+
+var presidents = [
+    var president44 = {
+        firstName = 'Barack', // Trump is not my president #sorrynotsorry
+        lastName = 'Obama',
+	    termLength = '4 years',
+		party = 'Democratic',
+		yearsofPresidency = '8',
+	};
+
+	var president43 = {
+	    firstName = 'George',
+	    lastName = 'Bush',
+		termLength = '4 years',
+		party = 'Republican',
+		yearsofPresidency = '8',
+	};
+
+	var president42 = {
+	    firstName = 'Bill',
+	    lastName = 'Clinton',
+		termLength = '4 years',
+		party = 'Democratic',
+		yearsofPresidency = '8',
+	};
+
+	var president41 = {
+	    firstName = 'George',
+	    lastName = 'Bush',
+		termLength = '4 years',
+		party = 'Republican',
+		yearsofPresidency = '4',
+	};
+
+	var president40 = {
+	    firstName = 'Ronald',
+	    lastName = 'Reagan',
+		termLength = '4 years',
+		party = 'Republican',
+		yearsofPresidency = '8',
+	};
+	];
